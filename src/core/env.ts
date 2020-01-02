cc985dac22b2ba149c129c029973a40b89009844
Jose Camelo Freitas <cmpjosf@outlook.com>
2020-01-02 14:23:49 -0300

    Parse bool and numbers in Env.Get() method

--- src/core/env.ts
+++ src/core/env.ts
@@ -1,15 +1,29 @@
+import { boolean } from "boolean";
 import { config } from "dotenv";
 
 config({
-  path: `${__dirname}/../.env`,
+  path: `${__dirname}/../../.env`,
 });
 
 export class Env {
+
   public static get(key: string, defaultValue?: string | number | boolean): any {
     const value = process.env[key];
 
     if (defaultValue !== undefined && (value === undefined || value === null)) {
       return defaultValue;
+    }
+
+    return Env.parse(value);
+  }
+
+  private static parse(value: any): any {
+    if (value === "true" || value === "false") {
+      return boolean(value);
+    }
+
+    if (!isNaN(Number(value))) {
+      return Number(value);
     }
 
     return value;
--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,3 @@
-import { boolean } from "boolean";
 import { Settings } from "src/interfaces/settings";
 import { Env } from "./core/env";
 
@@ -13,10 +12,10 @@ export const settings: Settings = {
     refreshExpires: 43200,
   },
   database: {
-    synchronize: boolean(Env.get("DB_SYNC", true)),
+    synchronize: Env.get("DB_SYNC", true),
     type: Env.get("DB_TYPE", "sqlite"),
     host: Env.get("DB_HOST"),
-    port: Number(Env.get("DB_PORT")),
+    port: Env.get("DB_PORT"),
     username: Env.get("DB_USER"),
     password: Env.get("DB_PASS"),
     database: Env.get("DB_NAME", ":memory:"),
